213b22fc4cfc8286ab9869b0a01e185aa871726d
natalie72 <46360156+natalie72@users.noreply.github.com>
2020-12-26 12:49:05 -0600

    Initial commit

---
+++ dist/Contenido.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+<?php include_once 'Encabezado.php';?>
+
+<body>
+    <!-- Navigation Bar-->
+    <header id="topnav">
+        <!-- MENU HORIZONTAL-->
+        <?php include_once 'Menu.php';?>
+    </header>
+    <!-- End Navigation Bar-->
+
+    <!-- ============================================================== -->
+    <!-- Start Page Content here -->
+    <!-- ============================================================== -->
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <!-- start page title -->
+            <div class="page-title-alt-bg"></div>
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Greeva</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">Dashboard</h4>
+            </div>
+            <!-- end page title -->
+        </div> <!-- end container -->
+    </div>
+    <!-- end wrapper -->
+
+    <!-- ============================================================== -->
+    <!-- End Page content -->
+    <!-- ============================================================== -->
+
+    <?php include_once 'Pie.php';?>
+</body>
+
+</html>
---
+++ dist/Menu.php
@@ -0,0 +1,338 @@
+<!-- Topbar Start -->
+<div class="navbar-custom">
+            <div class="container-fluid">
+                <ul class="list-unstyled topnav-menu float-right mb-0">
+
+                    <li class="dropdown notification-list">
+                        <!-- Mobile menu toggle-->
+                        <a class="navbar-toggle nav-link">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </a>
+                        <!-- End mobile menu toggle-->
+                    </li>
+                    <li class="dropdown notification-list">
+                        <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light"
+                            data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            <img src="assets/images/users/avatar-4.jpg" alt="user-image" class="rounded-circle">
+                            <span class="pro-user-name ml-1">
+                                Agnes K <i class="mdi mdi-chevron-down"></i>
+                            </span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h6 class="m-0">
+                                    Welcome !
+                                </h6>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="dripicons-user"></i>
+                                <span>My Account</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="dripicons-gear"></i>
+                                <span>Settings</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="dripicons-help"></i>
+                                <span>Support</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="dripicons-lock"></i>
+                                <span>Lock Screen</span>
+                            </a>
+
+                            <div class="dropdown-divider"></div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="dripicons-power"></i>
+                                <span>Logout</span>
+                            </a>
+
+                        </div>
+                    </li>
+
+                </ul>
+
+                <ul class="list-unstyled menu-left mb-0">
+                    <li class="float-left logo-box">
+                        <a href="index.html" class="logo">
+                            <span class="logo-lg">
+                                <img src="assets/images/logo-light.png" alt="" height="22">
+                            </span>
+                            <span class="logo-sm">
+                                <img src="assets/images/logo-sm.png" alt="" height="24">
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end Topbar -->
+<div class="topbar-menu">
+            <div class="container-fluid">
+                <div id="navigation">
+                    <!-- Navigation Menu-->
+                    <ul class="navigation-menu">
+
+                        <li class="has-submenu">
+                            <a href="index.html">
+                                <i class="dripicons-meter"></i>Dashboard
+                            </a>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#"> <i class="dripicons-briefcase"></i>UI Elements <div class="arrow-down"></div>
+                                </a>
+                            <ul class="submenu megamenu">
+                                <li>
+                                    <ul>
+                                        <li>
+                                            <a href="ui-buttons.html">Buttons</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-modals.html">Modals</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-tabs.html">Tabs & Accordions</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-notifications.html">Notifications</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-cards.html">Cards</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li>
+                                            <a href="ui-grid.html">Grid</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-general.html">General</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-typography.html">Typography</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-checkbox-radio.html">Checkbox & Radio</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-progress.html">Progress</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li>
+                                            <a href="ui-sweetalerts.html">Sweet Alerts</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-range-slider.html">Range Slider</a>
+                                        </li>
+                                        <li>
+                                            <a href="ui-ribbons.html">Ribbons</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#">
+                                <i class="dripicons-mail"></i>Email <div class="arrow-down"></div></a>
+                            <ul class="submenu">
+                                <li>
+                                    <a href="email-inbox.html">Inbox</a>
+                                </li>
+                                <li>
+                                    <a href="email-read.html">Read Email</a>
+                                </li>
+                                <li>
+                                    <a href="email-compose.html">Compose Email</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#">
+                                <i class="dripicons-jewel"></i>Components <div class="arrow-down"></div></a>
+                            <ul class="submenu">
+                                <li class="has-submenu">
+                                    <a href="calender.html">Calendar</a>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="#">Charts <div class="arrow-down"></div></a>
+                                    <ul class="submenu">
+                                        <li>
+                                            <a href="chart-flot.html">Flot Charts</a>
+                                        </li>
+                                        <li>
+                                            <a href="chart-chartist.html">Chartist Charts</a>
+                                        </li>
+                                        <li>
+                                            <a href="chart-chartjs.html">Chartjs Charts</a>
+                                        </li>
+                                        <li>
+                                            <a href="chart-knob.html">Jquery Knob</a>
+                                        </li>
+                                        <li>
+                                            <a href="chart-sparkline.html">Sparkline Charts</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="#">Forms <div class="arrow-down"></div></a>
+                                    <ul class="submenu">
+                                        <li>
+                                            <a href="form-elements.html">Form Elements</a>
+                                        </li>
+                                        <li>
+                                            <a href="form-advanced.html">Form Advanced</a>
+                                        </li>
+                                        <li>
+                                            <a href="form-validation.html">Form Validation</a>
+                                        </li>
+                                        <li>
+                                            <a href="form-wizard.html">Form Wizard</a>
+                                        </li>
+                                        <li>
+                                            <a href="form-summernote.html">Summernote</a>
+                                        </li>
+                                        <li>
+                                            <a href="form-upload.html">Form Upload</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="#">Tables <div class="arrow-down"></div></a>
+                                    <ul class="submenu">
+                                        <li>
+                                            <a href="tables-basic.html">Basic Tables</a>
+                                        </li>
+                                        <li>
+                                            <a href="tables-datatable.html">Data Tables</a>
+                                        </li>
+                                        <li>
+                                            <a href="tables-responsive.html">Responsive Tables</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="#">Icons <div class="arrow-down"></div></a>
+                                    <ul class="submenu">
+                                        <li>
+                                            <a href="icons-materialdesign.html">Material Design</a>
+                                        </li>
+                                        <li>
+                                            <a href="icons-simpleline.html">Simple Line</a>
+                                        </li>
+                                        <li>
+                                            <a href="icons-dripicons.html">Dripicons</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="#">Maps <div class="arrow-down"></div></a>
+                                    <ul class="submenu">
+                                        <li>
+                                            <a href="maps-google.html">Google Maps</a>
+                                        </li>
+                                        <li>
+                                            <a href="maps-vector.html">Vector Maps</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#"> <i class="dripicons-view-apps"></i>Layouts <div class="arrow-down"></div></a>
+                            <ul class="submenu">
+                                <li>
+                                    <a href="layouts-topbar-light.html">Topbar Light</a>
+                                </li>
+                                <li>
+                                    <a href="layouts-center-menu.html">Center Menu</a>
+                                </li>
+                                <li>
+                                    <a href="layouts-normal-header.html">Unsticky Header</a>
+                                </li>
+                                <li>
+                                    <a href="layouts-boxed.html">Boxed</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#"> <i class="dripicons-duplicate"></i>Pages <div class="arrow-down"></div></a>
+                            <ul class="submenu megamenu">
+                                <li>
+                                    <ul>
+                                        <li>
+                                            <a href="pages-login.html">Log In</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-register.html">Register</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-recoverpassword.html">Recover Password</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-lock-screen.html">Lock Screen</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-404.html">Error 404</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-500.html">Error 500</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li>
+                                            <a href="pages-invoice.html">Invoice</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-pricing.html">Pricing</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-starter.html">Starter</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-timeline.html">Timeline</a>
+                                        </li>
+                                        <li>
+                                            <a href="pages-faq.html">FAQs</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                    <!-- End navigation menu -->
+
+                    <div class="clearfix"></div>
+                </div>
+                <!-- end #navigation -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end navbar-custom -->
